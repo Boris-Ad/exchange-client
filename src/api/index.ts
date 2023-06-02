1c85601855d917e76b98facdde3479aa2546ccf0
Boris <adboris@yandex.ru>
2023-06-02 22:08:03 +0300

    new ngrok

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,6 @@
-import ky from 'ky'
-import { IDataToBot } from '../types'
+import ky from "ky";
+import { IDataToBot } from "../types";
 
-
-export const sendDataToBot = async (queryId:string,data:IDataToBot) => {
- return await ky.post('https://0f93-91-135-222-122.eu.ngrok.io/api/bot',{json:{queryId,data}}).json()
-}
+export const sendDataToBot = async (queryId: string, data: IDataToBot) => {
+  return await ky.post("https://3e18-91-135-222-122.ngrok-free.app/api/bot", { json: { queryId, data } }).json();
+};
